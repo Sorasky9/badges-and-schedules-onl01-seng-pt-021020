7275db433ce1fa83174e774808d7763d695b14da
Malcom Fetus <sora.vista@gmail.com>
2020-02-19 05:01:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def badge_maker(name)
+ return "Hello, my name is #{name}."
+
+end
+
+def batch_badge_creator(attendees)
+attendees.collect do |badges|
+   "Hello, my name is #{badges}."
+end
+
+end
+
+def assign_rooms(attendees)
+  room_assignments.collect do |attendees|
+    " #{room_assignments}"
+  end
+
+end
