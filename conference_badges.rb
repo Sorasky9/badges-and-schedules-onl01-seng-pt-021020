3c2c07e7d6f79f43f3260fcf78719cc9b96b6dc8
Malcom Fetus <sora.vista@gmail.com>
2020-02-19 05:49:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,8 +11,7 @@ end
 end
 
 def assign_rooms(attendees)
-room_assignments = ["1","2","3","4","5","6","7"]
-  room_assignments.collect do |attendees|
+  room_assignments.each_with_index do |attendees|
     " #{room_assignments}"
       end
 
