a9a619920ad5dc2f81f34807b770cf7615de9907
Malcom Fetus <sora.vista@gmail.com>
2020-02-19 06:24:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ end
 
 def assign_rooms(speakers)
 new_arr = []
-speakers.each_with_index do |name, i|
+speakers.each_with_index do |name, index|
   new_arr << "Hello, #{name}! You'll be assigned to room #{i + 1}!"
 end
 new_arr
